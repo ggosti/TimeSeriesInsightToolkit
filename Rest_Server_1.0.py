2fc09e6f673b236cca56d724441b2059c20b53d4
ggosti <giogosti@gmail,com>
2024-06-28 12:16:15 +0000

    measure duration varaince works

--- Rest_Server_1.0.py
+++ Rest_Server_1.0.py
@@ -1,4 +1,7 @@
 from flask import Flask, jsonify, request
+import numpy as np
+
+import timeSeriesInsightToolkit as tsi
 
 # Crea un'istanza della classe Flask e configura l'applicazione.
 app = Flask(__name__)
@@ -28,20 +31,21 @@ def addizione():
     somma = data['numero1'] + data['numero2']
     return jsonify({'risultato': somma})
 
-@app.route('/measure/duration/variance/<path>', methods=['GET'])
-def show_duration_varaince(path):
-    pathSes = '/var/www/html/records/'+path
-    # parse path to records
-    if '/' == pathSes[-1]:
-        pathSes = pathSes[:-1] #takeout the slash and than the folder name
-        print(pathSes)
-    if 'preprocessed-VR-sessions' in pathSes: 
-        pathSes = pathSes[:-len('/preprocessed-VR-sessions')] 
-        print(pathSes)
-        ids, fileNames, dfSs, df = tsi.readData(pathSesRec)
-    #print(dfSs)
 
+# function that measures duration and variance
+def measureDurationVariance(path):
+    pathSesRec = '/var/www/html/records/'+path
+
+    # parse path to records
+    #if '/' == pathSes[-1]:
+    #    pathSes = pathSes[:-1] #takeout the slash and than the folder name
+    #    print(pathSes)
+    #if 'preprocessed-VR-sessions' in pathSes: 
+    #    pathSes = pathSes[:-len('/preprocessed-VR-sessions')] 
+    #    print(pathSes)
     # get variables from records    
+    ids, fileNames, dfSs, df = tsi.readData(pathSesRec)
+    #print(dfSs)
     paths=tsi.getPaths(ids,dfSs,['posx','posy','posz'])
     _,x,y,z = np.vstack(paths).T
     #paths = tsi.getPaths(ids,dfSs,['dirx','diry','dirz'])
@@ -56,9 +60,27 @@ def show_duration_varaince(path):
         totTimes.append(totTime)
         totVar = np.var(x)+np.var(y)+np.var(z)
         totVars.append(totVar) 
-    
-    return jsonify({'duration': totTimes, 
-                    'variance': totVars})
+    return {'duration': totTimes,'variance': totVars}
+
+# function that generates REST API to measure duration and variance
+#@app.route('/measure/duration/variance/', methods=['GET'])
+#def measure_duration_varaince():
+#    return jsonify({'error: add group 1 and group 2':[]})
+
+#@app.route('/measure/duration/variance/<group2>', methods=['GET'])
+#def measure_duration_varaince(group2):
+#    path = f'proc/{group2}/preprocessed-VR-sessions'
+#    #return jsonify({'path':path})
+#    return jsonify(measureDurationVariance(path))
+
+@app.route('/measure/duration/variance/<group1>/<group2>', methods=['GET'])
+def measure_duration_varaince(group1,group2):
+    path = f'{group1}/{group2}/preprocessed-VR-sessions'
+    #return jsonify({'path':path})
+    return jsonify(measureDurationVariance(path))
 
 if __name__ == '__main__':
+    #path = 'proc/bfanini-20231026-kjtgo0m0w/preprocessed-VR-sessions'
+    #result = measureDurationVariance(path)
+    #print(result)
     app.run(debug=True)
