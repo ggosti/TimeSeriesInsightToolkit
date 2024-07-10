621776ebe4e6a1bb2093efe006630c80844b560b
Giorgio G <giogosti@gmail.com>
2024-07-10 17:21:35 +0200

    started front end 2

--- rawGroups.py
+++ rawGroups.py
@@ -1,7 +1,9 @@
+# rawGroups.py
+
 import os
 
 def read_all():
     path = '/var/www/html/records/raw'
     # get variables from records    
-    groups = os.listdit()
+    groups = os.listdir()
     return jsonify({'groups':groups})
