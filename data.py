f9d02f1290621c2343f92048c57ebd9697130d0b
Giorgio G <giogosti@gmail.com>
2024-07-11 15:45:04 +0200

    started front end 3

--- data.py
+++ data.py
@@ -2,8 +2,14 @@
 
 import os
 
-def read_all():
+def read_raw():
     path = '/var/www/html/records/raw'
+    # get variables from records    
+    groups = os.listdir(path)
+    return groups
+
+def read_proc():
+    path = '/var/www/html/records/proc'
     # get variables from records    
     groups = os.listdir(path)
     return groups
